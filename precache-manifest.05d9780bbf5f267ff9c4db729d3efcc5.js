9313d456933c75b3fefec1633d307b7206ccf8a1
miyazato-yuusaku <s19018@std.it-college.ac.jp>
2020-02-26 06:23:51 +0000

    Updates

---
+++ precache-manifest.05d9780bbf5f267ff9c4db729d3efcc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d179a6aa9f5e21545dcff503f7e92cdb",
+    "url": "/gurunavi/index.html"
+  },
+  {
+    "revision": "1bc93fa0eee04bfbd095",
+    "url": "/gurunavi/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9db9040e183a59b0eb72",
+    "url": "/gurunavi/static/js/2.14994050.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/gurunavi/static/js/2.14994050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc93fa0eee04bfbd095",
+    "url": "/gurunavi/static/js/main.427b4228.chunk.js"
+  },
+  {
+    "revision": "1158fbc9431267c749bb",
+    "url": "/gurunavi/static/js/runtime-main.62cc256a.js"
+  }
+]);
